f9c8f62b1c38741f737c4b3257db7df006832cc1
Franklyn Weber <franklyn.weber@cybg.com>
2022-04-17 17:41:38 +0100

    Bumped ButtonConfig to version 2

--- Sources/SearchBarNavigation/SearchResultsSection.swift
+++ Sources/SearchBarNavigation/SearchResultsSection.swift
@@ -61,19 +61,19 @@ public struct SearchResults<Content: Hashable>: Collection {
     }
     
     public mutating func updateSection(withIdentifier identifier: String, withNewContent content: [Content]) {
-        guard var section = section(forIdentifier: identifier), let sectionIndex = index(ofSection: section) else {
-            return
-        }
-        section.updateResults(content)
-        sections[sectionIndex] = section
+        self[identifier]?.results = content
     }
     
-    public mutating func appendSection(withIdentifier identifier: String, withAdditionContent content: [Content]) {
-        guard var section = section(forIdentifier: identifier), let sectionIndex = index(ofSection: section) else {
-            return
-        }
-        section.appendResults(with: content)
-        sections[sectionIndex] = section
+    public mutating func appendSection(withIdentifier identifier: String, withAdditionalContent content: [Content]) {
+        self[identifier]?.results.append(contentsOf: content)
+    }
+    
+    public mutating func update(section: SearchResultsSection<Content>) {
+        self[section.id] = section
+    }
+    
+    public mutating func updateSection(withIdentifier identifier: String, atIndex index: Int, withNewContent content: Content) {
+        self[identifier]?[index] = content
     }
     
     public mutating func clearSections(withIdentifiers identifiers: [String]? = nil) {
@@ -204,17 +204,17 @@ public struct SearchResultsSection<Content: Hashable>: Collection, Identifiable 
         self.viewConfig = viewConfig
     }
     
-    mutating func appendResults(with additionalItems: [Content]) {
+    public mutating func appendResults(with additionalItems: [Content]) {
         results.append(contentsOf: additionalItems)
         hasReceivedContent = true
     }
     
-    mutating func updateResults(_ results: [Content]) {
+    public mutating func updateResults(_ results: [Content]) {
         self.results = results
         hasReceivedContent = true
     }
     
-    mutating func clear() {
+    public mutating func clear() {
         results.removeAll()
         hasReceivedContent = false
     }
